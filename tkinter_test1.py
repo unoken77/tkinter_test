c5c5dbf7c255cd08959559be85b2bfe4857033ce
Kenichi <newunkn@gmail.com>
2020-03-08 22:09:57 +0900

    first commit

---
+++ tkinter_test1.py
@@ -0,0 +1,22 @@
+# 関数を紐づけたボタンを配置
+#--------------------------
+
+import tkinter
+import random
+import math
+root = tkinter.Tk()
+root.title('demo_Tkinter')
+root.geometry("400x400")
+
+# コンソールに"Button is clicked."を出力する関数
+def clicked():
+  print("Button is clicked.")
+  print("サイコロ"+str(random.randint(1, 6)))
+
+# ボタンの作成（text=ボタンに表示されるテキスト, command=押下時に呼び出す関数）
+button = tkinter.Button(root, text="ボタン", command=clicked)
+
+# ボタンの表示
+button.grid()
+
+root.mainloop()
