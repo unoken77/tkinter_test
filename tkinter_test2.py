31fbfcd27720884afdde42079a905a9e725b6630
Kenichi <newunkn@gmail.com>
2020-03-13 23:47:17 +0900

    button changed

--- tkinter_test2.py
+++ tkinter_test2.py
@@ -18,7 +18,7 @@ root.geometry("350x150")         # 画面サイズ
 # 入出力エリア
 label1 = tkinter.Label(text='InputData')  # 入力用ラベル
 label1.place(x=5,y=5)                     # ラベルの表示位置 
-textBox1 = tkinter.Entry(width=5)         # 入力用テキストボックス 
+textBox1 = tkinter.Entry(width=20)         # 入力用テキストボックス 
 textBox1.place(x=100, y=5)                # テキストボックス位置指定
 label2 = tkinter.Label(text='OutputData') # 出力用ラベル
 label2.place(x=100,y=50)                  # ラベル位置
@@ -26,7 +26,7 @@ label3 = tkinter.Label(text='')
 label3.place(x=100,y=70)
 textBox1.focus_set()                      # テキストボックスにフォーカス指定 
 btn = tkinter.Button(text='Go', command=func) # ボタン作成
-btn.pack()
+btn.pack(fill=tkinter.X, expand=1)
 textBox1.bind('<Return>', calc)           # Enterキーが押されイベント設定
 
 root.mainloop()                           # 画面を表示
